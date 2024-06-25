d5ad61d140c3a7bec9bbcd8fb56f68e325435d6b
Santosa26 <albertsantos199400@outlook.com>
2024-06-25 11:23:09 -0300

    End Code

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-        alert('Você clicou no botão!');
-    });
+    console.log('Document loaded');
 });
